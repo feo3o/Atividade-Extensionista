6e745abf9100a176d00c59e189b573c9a0a17b50
Felipe Santos <151412382+feo3o@users.noreply.github.com>
2025-04-22 08:33:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const diabetes = document.getElementById("diabetes");
-const colesterol = document.getElementById("colesterol");
-const postura = document.getElementById("postura");
-const depressao = document.getElementById("depressao");
-const diabetes_class = document.getElementById("diabetes_class")
-const colesterol_class = document.getElementById("colesterol_class");
-const postura_class = document.getElementById("postura_class");
-const depressao_class = document.getElementById("depressao_class");
-const update_page = document.getElementById("reload");
-const diabetes_start = document.getElementById("diabetes_start");
-const diabetes_pause = document.getElementById("diabetes_pause");
-const diabetes_timer = document.getElementById("diabetes_timer");
-
 Notification.requestPermission();
 
 function notify()
@@ -18,107 +5,41 @@ function notify()
     new Notification("O tempo acabou!");
 }
 
-diabetes.addEventListener("click", event => 
-{
-    diabetes_class.style.display = "block";
-
-});
-
-colesterol.addEventListener("click", event =>
-{
-    colesterol_class.style.display = "block";
-});
-
-postura.addEventListener("click", event =>
-{
-    postura_class.style.display = "block";
-});
-
-depressao.addEventListener("click", event =>
-{
-    depressao_class.style.display = "block";
-});
-
-reload.addEventListener("click", event =>
-{
-    location.reload();
-});
-
-let diabetes_timeleft = 10800000;
-let diabetes_interval;
-
-const diabetesTimerUpdate = () => {
-    const hours = Math.floor(diabetes_timeleft / (1000 * 60 * 60));
-    const diabetes_minutes = Math.floor(diabetes_timeleft / (1000 * 60) % 60);
-    const diabetes_seconds = diabetes_timeleft % 60;
-
-    diabetes_timer.innerHTML = `${hours.toString().padStart(2,"0")}:${diabetes_minutes.toString().padStart(2,"0")}:${diabetes_seconds.toString().padStart(2,"0")}`;
-}
-
-const diabetesStartTimer = () => {
-    diabetes_interval = setInterval(() => {
-        diabetes_timeleft--;
-        diabetesTimerUpdate();
-
-        if (diabetes_timeleft === 0)
-        {
-            clearInterval(interval);
-            notify("O tempo acabou!");
-            diabetes_timeleft = 10800000;
-            diabetesTimerUpdate();
-        }
-    }, 1000);
-}
-
-const diabetes_pauseTimer = () => clearInterval(diabetes_interval);
-
-const pomodoro_timer = document.getElementById("pomodoro_timer");
-const pomodoro_start = document.getElementById("start");
-const pomodoro_pause = document.getElementById("pause");
-const pomodoro_restart = document.getElementById("restart");
-let time_left = 1500;
+let timeleft = 10800000;
 let interval;
 
-const updateTimer = () => {
-    const minutes = Math.floor(time_left / 60);
-    const seconds = time_left % 60;
+const timerUpdate = () => {
+    const hours = Math.floor(timeleft / (1000 * 60 * 60));
+    const minutes = Math.floor(timeleft / (1000 * 60) % 60);
+    const seconds = timeleft % 60;
 
-    pomodoro_timer.innerHTML = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+    relogio.innerHTML = `${hours.toString().padStart(2,"0")}:${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
 }
 
 const startTimer = () => {
     interval = setInterval(() => {
-        time_left--;
-        updateTimer();
+        timeleft--;
+        timerUpdate();
 
-        if (time_left === 0)
+        if (timeleft === 0)
         {
             clearInterval(interval);
             notify("O tempo acabou!");
-            time_left = 1500;
-            updateTimer();
+            timeleft = 10800000;
+            timerUpdate();
         }
     }, 1000);
 }
 
-const stopTimer = () => clearInterval(interval);
-const resetTimer = () => {
-    clearInterval(interval);
-    time_left = 1500;
-    updateTimer();
-}
-
-pomodoro_start.addEventListener("click", startTimer);
-pomodoro_pause.addEventListener("click", stopTimer);
-pomodoro_restart.addEventListener("click", resetTimer);
-diabetes_start.addEventListener("click", diabetesStartTimer);
-diabetes_pause.addEventListener("click", diabetes_pauseTimer);
+const pauseTimer = () => clearInterval(interval);
+start.addEventListener("click", startTimer);
+pause.addEventListener("click", pauseTimer);
 
 const aviso = document.getElementById("aviso");
 const borda = document.getElementById("weather-container");
 
 function getWeather() {
-    const apiKey = '################';
+    const apiKey = 'COLOQUE SUA API AQUI';
     const city = document.getElementById('city').value;
 
     if (!city) {
@@ -163,26 +84,6 @@ function displayWeather(data) {
 
         tempDivInfo.innerHTML = temperatureHTML;
         weatherInfoDiv.innerHTML = weatherHtml;
-
-        if (temperature > 29)
-        {
-            aviso.innerHTML = `A temperatura hoje é alta, cuidado com a desidratação e com a baixa humidade.<br> Use protetor solar ou evite sair de casa durante o pico de temperatura.`
-            borda.style.border="5px solid red";
-            borda.style.boxShadow="10px 10px 100px red";
-        }
-        else if (temperature >20)
-        {
-            aviso.innerHTML = `A temperatura hoje está agradável.`
-            borda.style.border="5px solid yellow";
-            borda.style.boxShadow="10px 10px 100px yellow";
-        }
-        else
-        {
-            aviso.innerHTML = `Hoje a temperatura está baixa, cuidado com ventos fortes e chuvas.`;
-            borda.style.border="5px solid blue";
-            borda.style.boxShadow="10px 10px 100px blue";
-        }
-
     }
 }
 
